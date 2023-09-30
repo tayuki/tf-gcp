adea625511d49fe8c24dea2096fe491a5dee439a
taka <maclife.taka@gmail.com>
2023-09-30 23:19:10 +0900

    modify

--- gke.tf
+++ gke.tf
@@ -2,7 +2,7 @@ resource "google_container_cluster" "primary" {
   name     = "primary-${var.cluster_name}"
   location = "${var.location}"
 
-  remove_default_node_pool = true
+  remove_default_node_pool = false
   initial_node_count       = 1
 
   network = "${var.network}"
